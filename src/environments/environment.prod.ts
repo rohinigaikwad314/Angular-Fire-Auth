fbd8c187b5efa6509bb0a3bef203a8c754310892
rohinigaikwad314 <rohinigaikwad314@gmail.com>
2024-07-25 00:55:02 +0530

    firebase config added..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "...",
+    authDomain: "angular-fire-auth-82857.firebaseapp.com",
+    projectId: "angular-fire-auth-82857",
+    storageBucket: "angular-fire-auth-82857.appspot.com",
+    messagingSenderId: "...",
+    appId: "...",
+    measurementId: "..."
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
-    production: false, 
-firebaseConfig :{
-    apiKey: "AIzaSyDWbPTkRtepLxDdSo_PqbCjWhaaLJ5NDVI",
-    authDomain: "angular-fire-auth-82857.firebaseapp.com",
-    projectId: "angular-fire-auth-82857",
-    storageBucket: "angular-fire-auth-82857.appspot.com",
-    messagingSenderId: "181958788755",
-    appId: "1:181958788755:web:01530df331d4eb4c682f66",
-    measurementId: "G-5VCJSCRW3X"
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDWbPTkRtepLxDdSo_PqbCjWhaaLJ5NDVI',
+    authDomain: 'angular-fire-auth-82857.firebaseapp.com',
+    projectId: 'angular-fire-auth-82857',
+    storageBucket: 'angular-fire-auth-82857.appspot.com',
+    messagingSenderId: '181958788755',
+    appId: '1:181958788755:web:01530df331d4eb4c682f66',
+    measurementId: 'G-5VCJSCRW3X',
   },
-}
+};
